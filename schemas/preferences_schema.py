cfe5639f55aa7a19998456d4e4eb5fa955acc96b
sin <francinesayson59@gmail.com>
2025-03-20 15:08:26 +0800

    Remove unused folder

--- schemas/preferences_schema.py
+++
@@ -1,8 +0,0 @@
-# schemas/preferences_schema.py
-from marshmallow import Schema, fields
-
-class PreferencesSchema(Schema):
-    user_id = fields.Str(required=True)
-    preferences = fields.Dict(required=True)
-
-preferences_schema = PreferencesSchema()
